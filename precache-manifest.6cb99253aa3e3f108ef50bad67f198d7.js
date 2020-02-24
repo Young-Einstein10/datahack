5b2376d51ba5d5dc67841009e0999e79e7a24ba0
young-einstein10 <aabdulrahmanyusuf@gmail.com>
2020-02-24 10:11:08 +0100

    Updates

---
+++ precache-manifest.6cb99253aa3e3f108ef50bad67f198d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23cac14c500e6a6200e93aaf644e3c97",
+    "url": "/datahackathon/index.html"
+  },
+  {
+    "revision": "ddb27a1faf87fa589863",
+    "url": "/datahackathon/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "be0bf86a9d39afd55a55",
+    "url": "/datahackathon/static/css/main.39f33db0.chunk.css"
+  },
+  {
+    "revision": "ddb27a1faf87fa589863",
+    "url": "/datahackathon/static/js/2.fa25968e.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/datahackathon/static/js/2.fa25968e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be0bf86a9d39afd55a55",
+    "url": "/datahackathon/static/js/main.fcb25b69.chunk.js"
+  },
+  {
+    "revision": "4d6290d2e83c75ba0916",
+    "url": "/datahackathon/static/js/runtime-main.0cbc6ce9.js"
+  }
+]);
